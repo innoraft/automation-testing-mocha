c3477c81ad3c0f9a27163cb11814f7e98bc54f43
jasodeep <jasodeepchatterjee@gmail.com>
2018-02-13 18:47:00 +0530

    example

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var config = require('./_config.js');
+var request=require('request');
+
+
+request.get(config.zomato_baseurl + '/categories', {headers: {'user-key': config.zomato_api}},function(err,res,body){
+  
+  console.log(body);
+ 
+  
+});
