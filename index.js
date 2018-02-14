1718c22282e4f9c8547255cc65d9eae12bfee5f6
Sonali Agarwal <sonali.agarwal@innoraft.com>
2018-02-14 18:33:58 +0530

    csv file created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var config = require('./_config.js');
-var request=require('request');
+var request= require('request');
 
+request.get(config.zomato_baseurl + '/search', {headers: {'user-key': config.zomato_api}},function(err,res,body){
+     
+    if (!err && res.statusCode == 200) {
 
-request.get(config.zomato_baseurl + '/categories', {headers: {'user-key': config.zomato_api}},function(err,res,body){
-  
   console.log(body);
+var fs = require('fs');
+
+
+fs.writeFile('data/' + config.city + '.csv', body , function (err) {
+  if (err) {
+	
+    console.log('Some error occured - file either not saved or corrupted file saved.');
+  } else{
+    console.log('It\'s saved!');
+  }
+});
+ }
  
-  
 });
