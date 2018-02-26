e39fe8ee7a2c40b3a22d47aa857088f090232a14
Sonali Agarwal <sonali.agarwal@innoraft.com>
2018-02-26 11:09:17 +0530

    parsing

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ request.get(config.zomato_baseurl + '/search', {headers: {'user-key': config.zom
     if (!err && res.statusCode == 200) {
 
   console.log(body);
-var fs = require('fs');
-
-
-fs.writeFile('data/' + config.city + '.csv', body , function (err) {
+var fs = require('fs'); 
+var obj = JSON.parse(body);
+// document.getElementById("demo").innerHTML = obj.name;
+// console.log(obj.name);
+ fs.writeFile('data/' + config.city + '.csv', document.getElementById("demo").innerHTML = obj.name , function (err) {
   if (err) {
 	
     console.log('Some error occured - file either not saved or corrupted file saved.');
@@ -20,3 +21,4 @@ fs.writeFile('data/' + config.city + '.csv', body , function (err) {
  }
  
 });
+
