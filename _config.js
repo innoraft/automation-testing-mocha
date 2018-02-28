fcc5f4ec7beeddb2d5749ac17b8b8150cad69aa6
Sonali Agarwal <sonali.agarwal@innoraft.com>
2018-02-28 11:10:14 +0530

    modified

---
+++ _config.js
@@ -0,0 +1,21 @@
+_config = {
+  zomato_api: '8316e598b8ff025cf80fa870bc7f2563',
+  zomato_baseurl: 'https://developers.zomato.com/api/v2.1',
+  city: 'bangalore',
+zomato_enid: '592' ,
+zomato_enty: 'group' ,
+zomato_lat: '40.732013' ,
+zomato_lon: '-73.996155' ,
+cuisines: 'chinese' , 
+zomato_estab: '31' ,
+  start: '0' ,
+  count: '20' ,
+zomato_coll: '1' ,
+zomato_category:  '2' ,
+zomato_sort: 'rating' ,
+zomato_order: 'asc' 
+}
+
+module.exports = _config;
+
+
