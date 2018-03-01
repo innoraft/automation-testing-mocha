96edcbdcc118be1ad1f6de1fd21476d2a57e2d9b
Sonali Agarwal <sonali.agarwal@innoraft.com>
2018-03-01 18:40:27 +0530

    modify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var config = require('./_config.js');
 var request= require('request');
 
-request.get(config.zomato_baseurl + '/search', {headers: {'user-key': config.zomato_api} , parameters: {'city': config.city , 'entity_id': config.zomato_enid , 'entity_type': config.zomato_enty , 'lat': config.zomato_lat , 'lon': config.zomato_lon , 'cuisines': config.cuisines , 'establishment_type': config.zomato_estab , 'start': config.start , 'count': config.count , 'collection_id': config.zomato_coll , 'category': config.zomato_category , 'sort': config.zomato_sort , 'order': config.zomato_order},function(err,res,body){
+request.get(config.zomato_baseurl + '/search', {headers: {'user-key': config.zomato_api} , parameters: {'city': config.city , 'entity_id': config.zomato_enid , 'entity_type': config.zomato_enty , 'lat': config.zomato_lat , 'lon': config.zomato_lon , 'cuisines': config.cuisines , 'establishment_type': config.zomato_estab , 'start': config.start , 'count': config.count , 'collection_id': config.zomato_coll , 'category': config.zomato_category , 'sort': config.zomato_sort , 'order': config.zomato_order}},function(err,res,body){
      
     if (!err && res.statusCode == 200) {
 
@@ -10,14 +10,14 @@ var fs = require('fs');
 // var obj = JSON.parse(body);
 // document.getElementById("demo").innerHTML = obj.name;
 // console.log(obj.name);
-const Json2csvParser = require('json2csv').Parser;
-const fields = ['name', 'rating' , 'address'];
+// const Json2csvParser = require('json2csv').Parser;
+// const fields = ['name', 'rating' , 'address'];
  
-const json2csvParser = new Json2csvParser({ fields });
-const csv = json2csvParser.parse(body);
+// const json2csvParser = new Json2csvParser({ fields });
+// const csv = json2csvParser.parse(body);
  
-console.log(csv);
- fs.writeFile('data/' + config.city + '.csv', csv , function (err) {
+// console.log(csv);
+ fs.writeFile('data/' + config.city + '.csv', body , function (err) {
   if (err) {
 	
     console.log('Some error occured - file either not saved or corrupted file saved.');
@@ -25,7 +25,7 @@ console.log(csv);
     console.log('It\'s saved!');
   }
 });
- }}
+ }
 });
 
 
